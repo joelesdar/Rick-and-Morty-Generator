928f6d53265de325bca2a6b4eef1b4277a951d4e
Link <joel-e11@hotmail.com>
2022-05-10 16:44:22 -0500

    Cliente Apollo y Ghraphql conectados, recibiendo datos de la api, imprimiendo y mostrando personajes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Characters from "../components/Characters";
+
 const Index = () => {
-    return(
-        <div>
-            <h1>Rick and Morty App</h1>
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Rick and Morty Generator App</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Rick and Morty Generator</h1>
+
+        <div className={styles.grid}>
+          <Characters />
         </div>
-    );
+      </main>
+
+      <footer className={styles.footer}>
+      </footer>
+    </div>
+  );
 }
 
 export default Index;
