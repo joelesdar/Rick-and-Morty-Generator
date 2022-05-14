c92cabad9f72d49f240d86cf725f49c1a1bb7502
Link <joel-e11@hotmail.com>
2022-05-14 18:04:19 -0500

    Cambios para subir repositorio a produccion

---
+++ out/_next/static/GueWXnQWbx4KE2HNF6yNJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/642-e08b0de3cc9dc83e.js","static/chunks/pages/index-d607dda15bd3e28a.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
